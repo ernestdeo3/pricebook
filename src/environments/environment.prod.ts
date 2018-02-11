108e2d0a5cfef21f1f456696b7ba27e82ffe0931
TH MOK <mokth2007@gmail.com>
2018-02-12 00:50:35 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB8waxyGcmyqkGrVLCQr8cmDxMDco12kGw",
+    apiKey: "<api key>",
     authDomain: "mokprj-679f2.firebaseapp.com",
     databaseURL: "https://mokprj-679f2.firebaseio.com",
     projectId: "mokprj-679f2",
     storageBucket: "mokprj-679f2.appspot.com",
-    messagingSenderId: "893739970358"
+    messagingSenderId: ""
   }
 };
 
